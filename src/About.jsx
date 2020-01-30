32a5dde28d3d1db2cba8075aee09d1ff8a7e8161
Chi Huynh <contact@chihuynh.me>
2020-01-30 15:31:53 -0800

    added react bootstrap and made skeleton for mobile first development

--- src/About.jsx
+++ src/About.jsx
@@ -15,14 +15,6 @@ const About = (props) => {
 				</p>
 			</div>
 
-			<div className='introduction' id='contacts'>
-				<p>Contact:</p>
-				<p>Email: contact[at]chihuynh[dot]me</p>
-				<p>Github: <a href='https://github.com/Chiexplores' target="_blank" rel="noopener noreferrer">Link</a></p>
-				<p>LinkedIn: <a href='https://www.linkedin.com/in/chihuynh7/' target="_blank" rel="noopener noreferrer">Link</a></p>
-				<a href='chi-resume.pdf' download><h2>Download Resume</h2></a>
-			</div>
-
 		</>
 	);
 }
--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import DTR from './Dtr.jsx';
 import FlightLogger from './FlightLogger.jsx';
 import Navigation from './Navigation.jsx';
 import Header from './Header.jsx';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 import './App.scss';
 
@@ -31,20 +33,48 @@ function App() {
 
 
   return (
-    <div className='main-container'>
-      <header></header>
-      <Header className="header" />
-      <Navigation/>
+    <Container>
+      {/* Stack the columns on mobile by making one full-width and the other half-width */}
+      <Row>
+        <Col xs={12} md={8}>
+          <Header className="header" />
+          <Navigation />
+        </Col>
+      </Row>
+
+      {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+      <Row>
+        <Col xs={6} md={6}>
+          xs=6 md=4
+          ABOUT
       <aside>
-        <About />
-      </aside>
-      <main>
-        <h1>Projects</h1>
-        <Projects onClick={onClick} />
-        {main}
-      </main>
-      <footer>Made by Chi</footer>
-    </div>
+            <About />
+          </aside>
+        </Col>
+        <Col xs={6} md={6}>
+          xs=6 md=4
+      <div className='introduction' id='contacts'>
+            <p>Contact:</p>
+            <p>Email: contact[at]chihuynh[dot]me</p>
+            <p>Github: <a href='https://github.com/Chiexplores' target="_blank" rel="noopener noreferrer">Link</a></p>
+            <p>LinkedIn: <a href='https://www.linkedin.com/in/chihuynh7/' target="_blank" rel="noopener noreferrer">Link</a></p>
+            <a href='chi-resume.pdf' download><h2>Download Resume</h2></a>
+          </div>
+        </Col>
+      </Row>
+
+      {/* Columns are always 50% wide, on mobile and desktop */}
+      <Row>
+        <Col xs={12}>
+          <Projects onClick={onClick} />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} lg={true}>
+          {main}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
