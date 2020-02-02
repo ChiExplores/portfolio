67b989d99fea8bc8a1eada99f2eb467d5fa2d116
Chi Huynh <contact@chihuynh.me>
2020-02-01 17:01:53 -0800

    took contacts out of About component

---
+++ src/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+const Contact = () => {
+  return (
+    <>
+      <div className='introduction' id='contacts'>
+        <p>Contact:</p>
+        <p>Email: contact[at]chihuynh[dot]me</p>
+        <p>Github: <a href='https://github.com/Chiexplores' target="_blank" rel="noopener noreferrer">Link</a></p>
+        <p>LinkedIn: <a href='https://www.linkedin.com/in/chihuynh7/' target="_blank" rel="noopener noreferrer">Link</a></p>
+        <a href='chi-resume.pdf' download><h2>Download Resume</h2></a>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
+
+
+
+
