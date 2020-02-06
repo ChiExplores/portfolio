e0b122aa35252ed0148117896e32f81ef68d0e23
Chi Huynh <contact@chihuynh.me>
2020-02-06 12:59:51 -0800

    aligned button text

--- src/Project.jsx
+++ src/Project.jsx
@@ -8,35 +8,36 @@ import './Project.scss';
 
 const Project = (props) => {
   return (
-    <div className='project-menu'>
+    <div className='project-menu col'>
       {/* <div className='about-icon'>
         <button className='about' onClick={props.onClick} >
           <img src={logo} className="about-logo" alt="about" id='0' />
         </button>About
 			</div> */}
-      <div className='project-icon'>
+      <div className='col project-icon text-center' >
         <button className='flight-logger' onClick={props.onClick} >
           <img src={flightlogger} className="flight-logger-logo" alt="flight-logger" id='1' />
-        </button>	Flight Logger
+        </button>
+        Flight Logger
 			</div>
 
       <div className='project-icon'>
         <button className='dtr' onClick={props.onClick} >
           <img src={dtr} className="Dtr-logo" alt="dtr" id='2' />
         </button>
-        DTR
+          DTR
 			</div>
       <div className='project-icon'>
         <button className='2048' onClick={props.onClick}>
           <img src={project1} className="2048-logo" alt="2048" id='3' />
         </button>
-        2048
+          2048
 		</div>
       <div className='project-icon'>
         <button className='stardust-truce' onClick={props.onClick}>
           <img src={stardust} className="stardust-truce-logo" alt="stardust-truce" id='4' />
         </button>
-        Stardust Truce
+          <p>Stardust Truce</p>
 		</div>
     </div>
   );
