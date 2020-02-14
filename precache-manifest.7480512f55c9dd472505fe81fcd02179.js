9e9de95266f6aeea4473ddf3b96c709d67e6975f
Chi Huynh <contact@chihuynh.me>
2020-02-14 15:58:43 -0800

    Updates

---
+++ precache-manifest.7480512f55c9dd472505fe81fcd02179.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf2c08cc3b15b0ae92108a8fb94fe1d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b5ea679f4812e3c20f20",
+    "url": "/portfolio/static/css/main.26313368.chunk.css"
+  },
+  {
+    "revision": "ed022f974841adcc18aa",
+    "url": "/portfolio/static/js/2.c5959274.chunk.js"
+  },
+  {
+    "revision": "21a2a98d9df7cec5f3e2510ba837dd85",
+    "url": "/portfolio/static/js/2.c5959274.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5ea679f4812e3c20f20",
+    "url": "/portfolio/static/js/main.8dc279a5.chunk.js"
+  },
+  {
+    "revision": "b28f66a9265218dcc79c",
+    "url": "/portfolio/static/js/runtime-main.652d903d.js"
+  },
+  {
+    "revision": "3f7290c0c4567540f1cb7677028add35",
+    "url": "/portfolio/static/media/2048-demo.3f7290c0.gif"
+  },
+  {
+    "revision": "8d95fe35075ac6126a66168e428021c6",
+    "url": "/portfolio/static/media/2048.8d95fe35.ico"
+  },
+  {
+    "revision": "983ef34e1eccb5b589d251871fbe6d1b",
+    "url": "/portfolio/static/media/cloud-background.983ef34e.jpg"
+  },
+  {
+    "revision": "8aef3d4e4a32a873eb90f7d8550b9278",
+    "url": "/portfolio/static/media/dtr-demo.8aef3d4e.png"
+  },
+  {
+    "revision": "624e7c2056c30ae823c88e4c0ee5e188",
+    "url": "/portfolio/static/media/dtr.624e7c20.ico"
+  },
+  {
+    "revision": "76fe893aa94a2f3c14382c7c2e1cf568",
+    "url": "/portfolio/static/media/favicon-4.76fe893a.ico"
+  },
+  {
+    "revision": "4b037a442d5ff6e015a26a4ef72f0603",
+    "url": "/portfolio/static/media/flight-demo.4b037a44.png"
+  },
+  {
+    "revision": "b39430d4bbb108a6ac8f2c74ea7b5d86",
+    "url": "/portfolio/static/media/flight-logger.b39430d4.ico"
+  },
+  {
+    "revision": "43a77da7fb20f775582a239353b34a26",
+    "url": "/portfolio/static/media/logo.43a77da7.ico"
+  },
+  {
+    "revision": "7052c44127880839be81d832389ea71b",
+    "url": "/portfolio/static/media/sdt-demo.7052c441.gif"
+  }
+]);
